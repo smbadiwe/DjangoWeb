20db746c8eed7711965dc969488ab296f7f845dd
smbadiwe <somasystemsng@gmail.com>
2017-07-01 02:53:18 -0700

    Added a couple of tests

--- DjangoWeb/polls/models.py
+++ DjangoWeb/polls/models.py
@@ -7,7 +7,12 @@ class Question(models.Model):
     question_text = models.CharField(max_length = 200)
     pub_date = models.DateTimeField('date published')
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        """
+        Was this published recently?
+        """
+        now = timezone.now()
+        # self.pub_date is between now and now - 1day
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
     def __str__(self):
         return self.question_text
 
